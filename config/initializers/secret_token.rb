3269c3a63af2a83c3f3f27e4c97c8de6a8d34fd3
Thomas Lubitz <tom@verticalbrands.com>
2012-03-17 16:33:42 -0700

    initialized rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tunity::Application.config.secret_token = 'acd9f52733e77bc47cdf27edc64b9ea16f0b78426d46a68bb4209bfb7230b1991656b69d2482e703fd5febaedf6e61b5020d406ec0de15699dbab993a3f91c62'
